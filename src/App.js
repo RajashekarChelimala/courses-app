a4e86f4da4b7363aae7e8e7c4f66428bcc3ee03b
RajashekarChelimala <rajashekarchelimala1@gmail.com>
2023-06-30 17:31:08 +0530

    Commit: 30-06-2023

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-toastify/dist/ReactToastify.css'
+// import logo from './logo.svg';
 import './App.css';
-
+// import { Header } from './components/Header';
+// import {Button} from 'reactstrap'
+import {ToastContainer,toast} from 'react-toastify'
+import {Home} from './components/Home'
+import {Course} from './components/Course'
+import { AllCourses } from './components/AllCourses';
+import { AddCourse } from './components/AddCourse';
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer/>
+      <Home/>
+      {/* <Course course={{title:"Django Course",description:"This is just testing"}}/>
+      <Course course={{title:"Java Course",description:"This is just testing"}}/>
+       */}
+        <AllCourses/>
+        <AddCourse/>
     </div>
   );
 }
-
 export default App;
